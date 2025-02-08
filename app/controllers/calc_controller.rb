98168cf37b8a54850261d0feb1bbb6fac5991687
danieloh <56809357+danieloh01@users.noreply.github.com>
2025-02-08 20:04:27 +0000

    new commit

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -4,4 +4,36 @@ class CalcController < ApplicationController
     render({ :template => "calc_templates/home"})
   end
 
+  def newsquare
+    render({ :template => "calc_templates/newsquare"})
+  end
+
+  def squareresults
+    render({ :template => "calc_templates/squareresults"})
+  end
+
+  def squareroot
+    render({ :template => "calc_templates/squareroot"})
+  end
+
+  def squarerootresults
+    render({ :template => "calc_templates/squarerootresults"})
+  end
+
+  def payment
+    render({ :template => "calc_templates/payment"})
+  end
+
+  def paymentresults
+    render({ :template => "calc_templates/paymentresults"})
+  end
+
+  def random
+    render({ :template => "calc_templates/random"})
+  end
+
+  def randomresults
+    render({ :template => "calc_templates/randomresults"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calc", :action => "home"})
+
+  get("/square/new", { :controller => "calc", :action => "newsquare"})
+
+  get("/square/results", { :controller => "calc", :action => "squareresults"})
+
+  get("/square_root/new", { :controller => "calc", :action => "squareroot"})
+
+  get("/square_root/results", { :controller => "calc", :action => "squarerootresults"})
+
+  get("/payment/new", { :controller => "calc", :action => "payment"})
+
+  get("/payment/results", { :controller => "calc", :action => "paymentresults"})
+
+  get("/random/new", { :controller => "calc", :action => "random"})
+
+  get("/random/results", { :controller => "calc", :action => "randomresults"})
 end
